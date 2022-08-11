119f81e3e580959470737919c82f3c9eb062c157
kmpragmatic <kmeza@pragmatic.com.pe>
2022-08-10 23:29:06 -0500

    app for union

---
+++ union/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    # App information
+    'name': 'Union',
+    'summary': 'Productos Union.',
+    'version': '15.0.1',
+    'category': 'Tools',
+    'license': 'OPL-1',
+    'website': 'https://www.pragmatic.com.pe/',
+    'contributors': [
+        'Kelvin Meza <kmeza@pragmatic.com.pe>',
+    ],
+    'depends': [
+        'muk_rest'
+    ],
+    'external_dependencies': {},
+    'data': [
+    ],
+    'images': [
+    ],
+    'qweb': [
+    ],
+
+    'author': 'Pragmatic S.A.C',
+    'website': 'pragmatic.com.pe',
+    'maintainer': 'Pragmatic S.A.C.',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
